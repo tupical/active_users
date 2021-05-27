b5c361114f6159bc582c4dd9be27f5ec3fc70f17
tupical <tupical@gmail.com>
2021-05-27 19:40:44 +0300

    init

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-Hello
+<?php
+session_start();
+
+require('database.php');
+
+$conn = new mysqli($servername, $username, $password, $database);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "REPLACE INTO activities(sessid,last_action) VALUES ('".session_id()."','".time()."')";
+mysqli_query($conn,$sql);
+mysqli_close($conn);
+
+print_r(session_id());
+echo '<br>';
+print_r(time());
+?>  
